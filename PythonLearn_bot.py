30e625c17fa65d3da099763466c034a63ce5aa32
Shiganova Ekaterina <eshiganova@iflex.ru>
2021-01-23 11:07:20 +0300

    modificate message from user

--- PythonLearn_bot.py
+++ PythonLearn_bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot('1482586619:AAH11_5FnmgBjrkMf6zbJLp24ro33dXvnRM')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет" or message.text == "ппривет":
+    if message.text == "Привет" or message.text == "привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == '/help':
         bot.send_message(message.from_user.id, "Здесь можно найти учебные материалы для изучения языка программирования"
